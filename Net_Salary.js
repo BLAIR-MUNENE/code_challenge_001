10a40dd42312d340c12a27ff1b2ce9373d181893
blairtchalla821@gmail.com <blairtchalla821@gmail.com>
2024-04-02 13:11:13 +0300

    save files

--- Net_Salary.js
+++ Net_Salary.js
@@ -1,7 +1,7 @@
 // The function calculates an individuals Net salary
 function netSalary(Earning) {
   //The amount of tax to be paid is 10% if their Earning
-  const tax = (Earning * 10) / 100;
+  const tax = Earning * 0.1;
   //The netsalary is calculated by subtracting the amount of tax to be paid fro their earning
   netSalary = Earning - tax;
 }
