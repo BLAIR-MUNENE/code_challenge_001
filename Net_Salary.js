52ad0edf9ba51d4c4c08a9565d8b2050a187b3da
Blair Munene <161374353+BLAIR-MUNENE@users.noreply.github.com>
2024-04-02 13:17:57 +0300

    Update Net_Salary.js

--- Net_Salary.js
+++ Net_Salary.js
@@ -1,9 +1,9 @@
-// The function calculates an individual's Net salary
-function net salary(Earning) {
+// The function calculates an individuals Net salary
+function netSalary(Earning) {
   //The amount of tax to be paid is 10% of their Earning
   const tax = Earning * 0.1;
-  //The net salary is calculated by subtracting the amount of tax to be paid fro their earning
+  //The netsalary is calculated by subtracting the amount of tax to be paid from their earning
   netSalary = Earning - tax;
 }
-// It outputs the net salary
-return net salary;
+// It outputs the netsalary
+return netSalary;
