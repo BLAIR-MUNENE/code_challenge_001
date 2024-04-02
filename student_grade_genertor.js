eb397139bffa40e8e8cffc1b4ae99f84029d8f74
Blair Munene <161374353+BLAIR-MUNENE@users.noreply.github.com>
2024-04-02 13:20:04 +0300

    Update student_grade_genertor.js

--- student_grade_genertor.js
+++ student_grade_genertor.js
@@ -1,15 +1,16 @@
 // The function calculates a students grade based on their respective mark
-function calculateGrade(mark) {
+function Grade(mark) {
   // A grade is assigned according to the mark boundary it satisfies
   if (mark > 79 && mark <= 100) {
-    return "A"; // If mark is greater than 79, return 'A'
+    return "A"; // If the mark is greater than 79, return 'A'
   } else if (mark >= 60 && mark <= 79) {
-    return "B"; // If mark is between 60 and 79, return 'B'
+    return "B"; // If the mark is between 60 and 79, return 'B'
   } else if (mark >= 50 && mark <= 59) {
-    return "C"; // If mark is between 50 and 59, return 'C'
+    return "C"; // If the mark is between 50 and 59, return 'C'
   } else if (mark >= 40 && mark <= 49) {
-    return "D"; // If mark is between 40 and 49, return 'D'
+    return "D"; // If the mark is between 40 and 49, return 'D'
   } else {
-    return "E"; // If mark is less than 40, return 'E'
+    return "E"; // If the mark is less than 40, return 'E'
   }
 }
+return Grade; 
